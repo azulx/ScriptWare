73849cdecdc4ac14e5f972d4a6e1d74e8b15e441
AzuLX <erditllugiqi@gmail.com>
2017-10-08 11:31:50 +0100

    Update fecheck.lua

--- scripts/fecheck.lua
+++ scripts/fecheck.lua
@@ -1,1 +1,1 @@
-game.StarterGui:SetCore("SendNotification", {Title = "FilteringEnabled";Text = "FilteringEnabled: "..tostring(workspace.FilteringEnabled);Icon = "rbxassetid://1094169301";Duration = 2})
+game.StarterGui:SetCore("SendNotification", {Title = "FilteringEnabled";Text = "FilteringEnabled: "..tostring(workspace.FilteringEnabled);Icon = "rbxassetid://1094169301";Duration = 6})
